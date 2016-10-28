19a87560474f5cfd2ac12f53132e0d5a3c060392
Ansal <ansal@bssatech.com>
2016-10-28 13:10:36 +0530

    search component with properties

--- app/components/prescouter.module.ts
+++ app/components/prescouter.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { MaterializeDirective } from "angular2-materialize";
 
 import { PrescouterComponent }  from './prescouter.component';
@@ -7,7 +8,8 @@ import { SearchComponent } from './search.component';
 
 @NgModule({
   imports: [
-      BrowserModule
+      BrowserModule,
+      FormsModule
     ],
   declarations: [
       MaterializeDirective,
--- app/components/search.component.ts
+++ app/components/search.component.ts
@@ -4,4 +4,47 @@ import { Component } from '@angular/core';
     selector: 'search',
     templateUrl: './app/components/search.component.html'
 })
-export class SearchComponent { }
+export class SearchComponent {
+    places: String[] = [
+        'London',
+        'Amsterdam',
+        'Warsaw',
+        'Stockholm',
+        'Paris',
+        'Brussels',
+        'Prague',
+        'Moscow',
+        'Madrid',
+        'Geneva',
+        'Budapest',
+        'Kiev',
+        'Lisbon',
+        'Rome',
+        'Athens',
+        'Istanbul'
+    ];
+
+    // Departure & Arrival selects
+    selectedFrom:String = '';
+    selectedTo:String = '';
+
+    // Filter
+    routeFilter = 'cheapest';
+
+    ngOnInit() {
+    }
+
+    reset() {
+        this.selectedFrom = '';
+        this.selectedTo = '';
+        this.routeFilter = 'cheapest';
+    }
+
+    swapFromAndTo() {
+        [this.selectedFrom, this.selectedTo] = [this.selectedTo, this.selectedFrom];
+    }
+
+    search() {
+        console.log('Searching');
+    }
+}
